7373c3734d1b4970135fa9f60bee27f7c1159f61
olga <olenbka@inbox.ru>
2021-06-09 13:06:19 +0200

    second

---
+++ constants/catalog.js
@@ -0,0 +1,63 @@
+const CATALOG = [
+{
+    id: 'el1',
+    name: 'Tshirt',
+    img: 'https://i.ibb.co/jhgCGMk/tshirt.jpg',
+    price: 1500   
+
+},
+{
+    id: 'el2',
+    name: 'Jacket',
+    img: 'https://i.ibb.co/xXpyx3M/jacket.jpg',
+    price: 3500 
+
+},
+{
+    id: 'el3',
+    name: 'Socks',
+    img: 'https://i.ibb.co/hY1wXFs/socks.jpg',
+    price: 200 
+},
+{
+    id: 'el4',
+    name: 'Jeans',
+    img: 'https://i.ibb.co/pKgwrLW/100417-jeans-madewell-embed.jpg'
+},
+{
+    id: 'el5',
+    name: 'Rock',
+    img: 'https://i.ibb.co/C13gBHR/0ca09a908e0557f8e6a66ca01c639fc3-1221-1607.jpg',
+    price: 2200 
+},
+{
+    id: 'el6',
+    name: 'Coat',
+    img: 'https://i.ibb.co/3d8GnCc/ebf3bef251b4c6412b45bc810eead1e7.jpg',
+    price: 7000 
+},
+{
+    id: 'el7',
+    name: 'Dress',
+    img: 'https://i.ibb.co/q5Cgj2L/4393441805-1-1-3.jpg',
+    price: 4200 
+},
+{
+    id: 'el8',
+    name: 'Hemd',
+    img: 'https://i.ibb.co/XYwy7WJ/hemd.jpg',
+    price: 4100 
+},
+{
+    id: 'el9',
+    name: 'Shoes',
+    img: 'https://i.ibb.co/P54HVvJ/shoes.jpg',
+    price: 4900 
+},
+{
+    id: 'el10',
+    name: 'Shorts',
+    img: 'https://i.ibb.co/dg17Cjw/45f619ae1a080a5683598c84ec712462-468-615.jpg',
+    price: 3300 
+},
+]
